a7a6251a0d30ca2411bfc30b949e28a9c6ed4ceb
Kristen Han <yeonseoo@gmail.com>
2026-02-02 16:22:44 +0900

    Initial Commit

---
+++ Ping!/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ping!
+//
+//  Created by Yeonseo Han on 2/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ping!/Ping_App.swift
@@ -0,0 +1,17 @@
+//
+//  Ping_App.swift
+//  Ping!
+//
+//  Created by Yeonseo Han on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Ping_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
